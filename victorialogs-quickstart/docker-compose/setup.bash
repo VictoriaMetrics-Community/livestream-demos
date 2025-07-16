d98ebc110f72317fb4cc9e651e43d8e699577c8a
Mathias Palmersheim <mathias@victoriametrics.com>
2025-07-16 13:50:25 -0500

    Fixed setup script

--- victorialogs-quickstart/docker-compose/setup.bash
+++ victorialogs-quickstart/docker-compose/setup.bash
@@ -1,4 +1,8 @@
-#!/bin/bash
-git clone https://github.com/VictoriaMetrics/victoriaMetrics
-cd victoriametrics/deplyment/docker
-docker compose up -d -f docker-compose-victorialogs.yml
+#!/bin/env bash
+if [ ! -d "victoriaMetrics" ]; then
+  git clone https://github.com/VictoriaMetrics/victoriaMetrics
+fi
+cd victoriaMetrics/deployment/docker
+rm -f docker-compose.yml
+ln -s docker-compose-victorialogs.yml docker-compose.yml
+docker compose up -d
